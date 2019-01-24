15ba839eb94233a4ac1fa8b632ec0e7931fbde6a
Dermot <dermot.hughes@rewardinsight.com>
2019-01-24 16:36:54 +0000

    Form stateless component

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
-import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
 import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
@@ -11,6 +12,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
 @NgModule({
     declarations: [
         PassengerDashboardComponent,
+        PassengerViewerComponent,
         PassengerCountComponent,
         PassengerDetailComponent
     ],
@@ -19,7 +21,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
         HttpModule
     ],
     exports: [
-        PassengerDashboardComponent
+        PassengerViewerComponent
     ],
     providers: [
         PassengerDashboardService
--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -20,6 +20,13 @@ export class PassengerDashboardService {
       .catch((error: any) => Observable.throw(error.json()));
   }
 
+  getPassenger(id: number): Observable<Passenger> {
+    return this.http
+      .get(`${PASSENGER_API}/${id}`)
+      .map((response: Response) => response.json())
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
   updatePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http
       .put(`${PASSENGER_API}/${passenger.id}`, passenger)
